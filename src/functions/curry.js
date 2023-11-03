d0ae12d9b449ed7999cb360499f8dccf53c3ed8a
Arsen Astsatryan <arsen@Arsens-MacBook-Pro.local>
2023-11-03 20:19:45 +0400

    Curry function fixes

--- src/functions/curry.js
+++ src/functions/curry.js
@@ -1,26 +1,20 @@
 /**
  * Curry function to transform a function that takes multiple arguments
- * into a series of unary functions.
+ * into a series of functions.
  * @param {Function} fn - The function to be curried.
  * @returns {Function} - The curried version of the original function.
  */
 function curry(fn) {
-  if (typeof fn !== "function") {
-    throw new Error(
-      "The curry function requires a valid function as its first argument."
-    );
-  }
+    let arity = fn.length;
 
-  let arity = fn.length;
+    return function curriedFunction(...args) {
+        if (args.length >= arity) {
+            return fn(...args);
+        }
 
-  return function curriedFunction(...args) {
-    if (args.length >= arity) {
-      return fn(...args);
-    }
-
-    return function (...nextArgs) {
-      return curriedFunction(...args, ...nextArgs);
+        return function (...nextArgs) {
+            return curriedFunction(...args, ...nextArgs);
+        };
     };
-  };
 }
 export default curry;
