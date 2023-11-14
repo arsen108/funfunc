c9462687e3f35a2e3152fe8e7f46260c111b8ac5
Arsen Astsatryan <arsen@Arsens-MacBook-Pro.local>
2023-11-15 00:43:17 +0400

    Added library usage configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,41 @@
 import babel from 'rollup-plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'umd',
-    name: 'funfunc'
+import pkg from './package.json'
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: pkg.jsdelivr,
+        format: 'umd',
+        name: 'F',
+        exports: 'named'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelrc: true
+      }),
+      terser()
+    ]
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: true
-    })
-  ]
-}
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+]
