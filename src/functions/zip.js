54ad0decac59f6d43beecfe52c170cb9424177e0
arsen108 <arsen.astsatryan@gmail.com>
2023-11-21 22:55:26 +0400

    Added zip function with tests

---
+++ src/functions/zip.js
@@ -0,0 +1,20 @@
+/**
+ * Zips two arrays together by combining corresponding elements into pairs.
+ * The length of the returned array is equal to the shorter of the two input arrays.
+ *
+ * @param {Array} a - The first array to zip.
+ * @param {Array} b - The second array to zip.
+ * @returns {Array<Array>} An array of arrays, where each inner array contains elements from the same index of `a` and `b`.
+ */
+function zip(a, b) {
+    const len = Math.min(a.length, b.length);
+    const zippedArray = new Array(len);
+
+    for (let i = 0; i < len; i++) {
+        zippedArray[i] = [a[i], b[i]];
+    }
+
+    return zippedArray;
+}
+
+export default zip;
---
+++ tests/zip.test.js
@@ -0,0 +1,15 @@
+import * as F from "../src";
+
+describe("zip", () => {
+    test("zips two arrays by combining corresponding elements into pairs", () => {
+        const arr1 = [1, 2, 3];
+        const arr2 = ["a", "b", "c"];
+        const zippedArray = [
+            [1, "a"],
+            [2, "b"],
+            [3, "c"],
+        ];
+
+        expect(F.zip(arr1, arr2)).toStrictEqual(zippedArray);
+    });
+});
