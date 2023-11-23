3830c64f5482b143c59bff2f3e07fc0b5f12d0cc
arsen108 <arsen.astsatryan@gmail.com>
2023-11-24 00:14:25 +0400

    Transduce test fix

--- tests/transduce.test.js
+++ tests/transduce.test.js
@@ -1,25 +1,25 @@
-import * as F from '../src'
+import * as F from "../src";
 
-describe('transduce', () => {
-  test('should tranduce on provided collection with filter transducer', () => {
-    const arr = Array.from({ length: 12 }, (_, i) => i + 1)
+describe("transduce", () => {
+    test("should tranduce on provided collection with filter transducer", () => {
+        const arr = Array.from({ length: 12 }, (_, i) => i + 1);
 
-    const isEven = (n) => n % 2 === 0
-    const filterTransducer = F.filterTransducer(isEven)
-    const reducer = (acc, val) => acc + val
-    const result = F.tranduce(filterTransducer, reducer, 0, arr)
+        const isEven = (n) => n % 2 === 0;
+        const filterTransducer = F.filterTransducer(isEven);
+        const reducer = (acc, val) => acc + val;
+        const result = F.transduce(filterTransducer, reducer, 0, arr);
 
-    expect(result).toStrictEqual(42)
-  })
+        expect(result).toStrictEqual(42);
+    });
 
-  test('should tranduce on provided collection with map transducer', () => {
-    const arr = [1, 2, 3, 4, 5]
+    test("should tranduce on provided collection with map transducer", () => {
+        const arr = [1, 2, 3, 4, 5];
 
-    const multiply = (a, b) => a * b
-    const mapTransducer = F.mapTransducer(F.curry(multiply)(2))
-    const reducer = (acc, val) => acc + val
-    const result = F.tranduce(mapTransducer, reducer, 0, arr)
+        const multiply = (a, b) => a * b;
+        const mapTransducer = F.mapTransducer(F.curry(multiply)(2));
+        const reducer = (acc, val) => acc + val;
+        const result = F.transduce(mapTransducer, reducer, 0, arr);
 
-    expect(result).toStrictEqual(30)
-  })
-})
+        expect(result).toStrictEqual(30);
+    });
+});
